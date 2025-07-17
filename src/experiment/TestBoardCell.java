ae69e8c448182c8d30ae105dce29d7b53203b5b1
shauryasaxenas <shaurya.saxena03@gmail.com>
2025-07-17 14:21:45 -0600

    Created class outline

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,40 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row; 
+	private int column;
+	
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		return;
+	}
+	
+	public Set<TestBoardCell> getAdjList() {
+		return new HashSet<>();
+	}
+	
+	public void setRoom(boolean isRoom) {
+		return;
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	public void setOccupied(boolean occupied) {
+		return;
+	}
+	
+	public boolean getOccupied() {
+		return false;
+	}
+	
 }
