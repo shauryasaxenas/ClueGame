00a9546301f60042e393459886386da517161820
shauryasaxenas <shaurya.saxena03@gmail.com>
2025-07-17 14:27:31 -0600

    Created class skeleton

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,23 @@
 package experiment;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestBoard {
 
+	public TestBoard() {
+		super();
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		return;
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return new TestBoardCell(row, col);
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		return new HashSet<>();
+	}
 }
