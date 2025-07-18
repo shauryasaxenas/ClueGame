c873ca65e66c67fff1ddcfc3f193dea9f73cf9f0
shauryasaxenas <shaurya.saxena03@gmail.com>
2025-07-17 21:47:46 -0600

    Created first test

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -2,13 +2,27 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import experiment.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.Set;
 
 class BoardTestsExp {
-
+	private TestBoard board;
+	
+	@BeforeEach
+	public void setup() {
+		board = new TestBoard();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAdjacencyTopLeft() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> list = cell.getAdjList();
+		
+		assertEquals(2, list.size());
+		assertTrue(list.contains(board.getCell(0, 1)));
+		assertTrue(list.contains(board.getCell(1, 0)));
 	}
 
 }
