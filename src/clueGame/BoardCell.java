d6b5377efa5768bd7f38ac24eadefa1724b485a4
shauryasaxenas <shaurya.saxena03@gmail.com>
2025-07-21 14:51:05 -0600

    Followed UML and created class skeletons

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -4,77 +4,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BoardCell {
-	private int row, col;
+	private int row;
+	private int col;
 	private char initial;
-	private DoorDirection doorDirection = DoorDirection.NONE;
-	private boolean roomLabel, roomCenter = false;
+	private DoorDirection doorDirection;
+	private boolean roomLabel;
+	private boolean roomCenter;
 	private char secretPassage;
 	private Set<BoardCell> adjList = new HashSet<>();
-
-	public BoardCell(int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
 	
-	public int getRow() {
-		return row;
-	}
-	
-	public int getCol() {
-		return col;
-	}
-	
-	public int getInitial() {
-		return initial;
-	}
-	
-	public DoorDirection getDoorDirection() {
-		return doorDirection;
-	}
-	
-	public boolean isLabel() {
-		return roomLabel;
-	}
-	
-	public boolean isRoomCenter() {
-		return roomCenter;
-	}
-	
-	public Character getSecretPassage() {
-		return secretPassage;
-	}
-	
-	public Set<BoardCell> getAdjList() {
-		return adjList;
-	}
-	
-	public boolean isDoorway() {
-		return doorDirection != DoorDirection.NONE;
-	}
-	
-	
-
-	public void setInitial(char initial) {
-		this.initial = initial;
-	}
-
-	public void setDoorDirection(DoorDirection direction) {
-		this.doorDirection = direction;
-	}
-
-	public void setRoomLabel(boolean label) {
-		this.roomLabel = label;
-	}
-
-	public void setRoomCenter(boolean center) {
-		this.roomCenter = center;
-	}
-
-	public void setSecretPassage(char secretPassage) {
-		this.secretPassage = secretPassage;
-	}
-
-	public void addAdjacency(BoardCell cell) {
-		adjList.add(cell);
+	public void addAdj(BoardCell adj) {
+		adjList.add(adj);
 	}
 }
--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -4,29 +4,4 @@ public class Room {
 	private String name;
 	private BoardCell centerCell;
 	private BoardCell labelCell;
-	
-	public Room(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-		
-	public void setCenterCell(BoardCell centerCell) {
-		this.centerCell = centerCell;
-	}
-	
-	public void setLabelCell(BoardCell labelCell) {
-		this.labelCell = labelCell;
-	}
-	
-	public BoardCell getCenterCell() {
-		return centerCell;
-	}
-
-	public BoardCell getLabelCell() {
-		return labelCell;
-	}
-
 }
